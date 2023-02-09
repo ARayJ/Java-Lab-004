f340f119cc2d38f213265275a1078c538865e0b0
ARayJ <AaronRayJohnson@gmail.com>
2023-02-08 18:35:49 -0800

    Completed Lab 004!

--- src/SimpleMethod.java
+++ src/SimpleMethod.java
@@ -1,6 +1,9 @@
+import java.lang.Math;
 public class SimpleMethod {
     // Put your method code here
-
+    public static void square(Integer i) {
+        System.out.println(Math.sqrt(i));
+    }
     public static void main(String[] args) {
         // This is the method call, create the square method.
         square(25);
